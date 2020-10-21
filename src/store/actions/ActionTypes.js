2e64b8605d59dd394a3d3b540859b803826027e8
Amirdharajan M <amirtharajan1990@gmail.com>
2020-10-21 17:59:43 +0530

    Completed

--- src/store/actions/ActionTypes.js
+++ src/store/actions/ActionTypes.js
@@ -1,5 +1,5 @@
 export const MAIN_SLIDE = 'MAIN_SLIDE';
 export const GALLERY = 'GALLERY';
 
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = 'https://lavanyashenna-backend.herokuapp.com';
 export const AUTH_TOKEN = localStorage.getItem('');
