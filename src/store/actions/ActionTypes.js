9e8d824ae2be89a072e99eb9c5b485931bbf49ec
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2020-10-22 23:33:03 +0530

    Update ActionTypes.js

--- src/store/actions/ActionTypes.js
+++ src/store/actions/ActionTypes.js
@@ -3,6 +3,6 @@ export const GALLERY = 'GALLERY';
 export const BRIDAL_PACKAGE_IMAGE = 'BRIDAL_PACKAGE_IMAGE';
 
 
-// export const BASE_URL = 'https://lavanyashenna-backend.herokuapp.com';
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = 'https://lavanyashenna-backend.herokuapp.com';
+// export const BASE_URL = 'http://127.0.0.1:8000';
 export const AUTH_TOKEN = localStorage.getItem('');
